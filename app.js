77cc37c9a05c69a78a56c5be87f4ea504d685baa
k3v-0 <k3v-0@github.com>
2017-12-29 20:40:35 -0800

    updated to-do-app delete button

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  const addToDoForm = document.getElementById("addToDoForm");
+  const newToDoText = document.getElementById("newToDoText");
+  const toDoList = document.getElementById("toDoList");
+  const deleteButton = document.getElementById("deleteButton");
 
-  addToDoForm.addEventListener('submit', () => {
+    // add event listener to addToDoForm to listen for the submit event
+  addToDoForm.addEventListener("submit", (event) => {
+
+    // prevent default behavior of page reload when submit event is triggered
     event.preventDefault();
 
-    // get the text
+    // get the text input when submit event is triggered
     let title = newToDoText.value;
 
     //create a new li
-    let newLi = document.createElement('li');
+    let newLi = document.createElement("li");
 
     //create a new li
-    let checkbox = document.createElement('input');
+    let checkbox = document.createElement("input");
 
     //create remove button
-    let button = document.createElement('button');
+    let removeButton = document.createElement("button");
 
-    //create button text
-    let btnName = document.createTextNode('remove');
+    // //create button text
+    // let btnName = document.createTextNode("remove");
 
     // set the input's type to checkbox
     checkbox.type = "checkbox";
@@ -30,17 +34,34 @@ function onReady() {
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
-    // attach the button to the li
-    newLi.appendChild(button);
+    // // attach the button to the li
+    // newLi.appendChild(removeButton);
 
     // attach text to button
-    button.appendChild(btnName);
+    // removeButton.appendChild(btnName);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     //empty the input
-    newToDoText.value = '';
+    newToDoText.value = "";
+
+
+});
+
+deleteButton.addEventListener("click", event => {
+
+    let toDos = document.getElementsByTagName("li");
+
+    for (var i = toDos.length - 1; i >= 0; i--) {
+
+      if (toDos[i].children[0].checked == true) {
+
+        toDos[i].remove();
+
+      }
+
+    }
 
   });
 }
